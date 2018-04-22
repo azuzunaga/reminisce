2345fcdf71f706b8306bb5defd169c9e8ade39a6
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-22 12:23:02 -0700

    Add oath routes for logging in and out

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const routes = require("./routes");
+const keys = require("./config/keys");
+require("./models");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app);
 
 app.get("/", (req, res) => res.send("hi there"));
 
