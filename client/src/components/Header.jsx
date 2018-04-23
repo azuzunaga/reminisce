f4a508bf5a155cf650209e41bfa7ed8cf273a715
Americo <ame.zuz@gmail.com>
2018-04-22 21:56:30 -0700

    Add header with login button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,18 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">
+            Reminisce
+          </a>
+          <ul className="right">
+            <li>
+              <a href="/auth/google">Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
