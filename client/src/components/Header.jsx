9841b31faa1acf415eeb8e0b53cb9f6a7b00e0a8
Americo <ame.zuz@gmail.com>
2018-04-22 23:34:36 -0700

    Handle link routing when logged in/out

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   renderContent() {
@@ -35,9 +36,12 @@ class Header extends React.Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/dashboard" className="left brand-logo">
+          <Link
+            to={this.props.auth ? "/dashboard" : "/"}
+            href="/dashboard" className="left brand-logo"
+          >
             Reminisce
-          </a>
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
