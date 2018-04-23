b2896fee80a15a5c575679a633c75bd5372cbd53
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-23 10:38:21 -0700

    Add AuthRoute and ProtectedRoute and use them in App.jsx

---
+++ client/src/utils/authRoutes.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Route, Redirect, withRouter } from "react-router-dom";
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={props =>
+      !loggedIn ? <Component {...props} /> : <Redirect to="/dashboard" />
+    }
+  />
+);
+
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={props =>
+      loggedIn ? <Component {...props} /> : <Redirect to="/" />
+    }
+  />
+);
+
+const mapStateToProps = state => {
+  return { loggedIn: state.auth !== false };
+};
+
+export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
