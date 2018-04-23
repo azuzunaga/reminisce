f70d63300df28f4b5c7866c31af197503b11c7bb
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-23 13:34:06 -0700

    Updated styling on splash page

--- client/src/components/Splash.jsx
+++ client/src/components/Splash.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import '../styles/splash.css';
+import logo from '../assets/reminisceAlexBrushWhite.png';
 const Splash = () => {
   return (
-    <div>
-      <h1>Reminisce</h1>
-      <h4>
-        The tool that writers can't live without.
-        Maintain countless drafts and still maintain your sanity.
-      </h4>
-      <a href="/auth/google">Sign in with Google</a>
-    </div>
+    <div className="splash-container">
+        <h1 className="our-logo"><img src={logo} alt="Reminisce Logo"/></h1>
+        <h4 className="welcome-text">
+          The tool that writers can't live without.
+          Maintain countless drafts and still maintain your sanity.
+        </h4>
+        <a className="google-signin"
+          href="/auth/google">Sign in with Google
+        </a>
+      </div>
   );
 };
 
