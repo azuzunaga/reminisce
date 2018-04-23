ebb550d9585cd42915795163bde57d5dafd7582f
Americo <ame.zuz@gmail.com>
2018-04-22 23:34:21 -0700

    Move splash component to its own file

---
+++ client/src/components/Splash.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Splash = () => {
+  return (
+    <div>
+      <h1>Reminisce</h1>
+      <h4>
+        The tool that writers can't live without.
+        Maintain countless drafts and still maintain your sanity.
+      </h4>
+    </div>
+  );
+};
+
+export default Splash;
