2fc8a7085b414e2d0a412b656b80d79aa271631d
Americo <ame.zuz@gmail.com>
2018-04-22 23:05:34 -0700

    Handle user state on header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Sign in with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="api/logout">Sign Out, {this.props.auth.firstName}</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="left brand-logo">
+          <a href="/dashboard" className="left brand-logo">
             Reminisce
           </a>
           <ul className="right">
-            <li>
-              <a href="/auth/google">Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +37,8 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
