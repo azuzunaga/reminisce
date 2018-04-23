c4c52aeb3bb83ea71bb7034823e969f5cb0ba07e
Americo <ame.zuz@gmail.com>
2018-04-22 23:21:13 -0700

    Redirect users on sign in an sign out

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,11 +8,17 @@ module.exports = app => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect('/dashboard');
+    }
+  );
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get("/api/current_user", (req, res) => {
