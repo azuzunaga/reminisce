5b3a9f69ec5ee4e64c3b8f3dfaa7214539a55e72
Americo <ame.zuz@gmail.com>
2018-04-22 20:13:17 -0700

    Create store with placeholder reducer and state, use Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 
+const { logger } = require('redux-logger');
+const middlewares = [thunk, logger];
+
+const store = createStore(() => [], {}, applyMiddleware(...middlewares));
+
 ReactDOM.render(
-  <App />, document.getElementById("root")
+  <Provider store={store}><App /></Provider>,
+  document.getElementById("root")
 );
