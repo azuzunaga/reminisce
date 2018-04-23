b96b31aaf06c9c563af4aafb514bcf0c3b7ed137
Americo <ame.zuz@gmail.com>
2018-04-22 21:37:02 -0700

    Move header to its own component file

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        Header
+      </div>
+    );
+  }
+}
+
+export default Header;
