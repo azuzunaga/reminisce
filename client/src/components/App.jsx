a108bf4b727bdade560ea5da19c99cf9293e55de
Americo <ame.zuz@gmail.com>
2018-04-22 18:21:39 -0700

    Create functional component, add link to sign in with Google

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <p>Hai and welcome to Reminisce</p>
+      <a href="/auth/google">Sign In with Google</a>
+    </div>
+  );
+};
+
+export default App;
