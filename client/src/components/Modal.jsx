f60fae2519b5e8b9d1c09502b57aaeb9c8be07bd
Americo <ame.zuz@gmail.com>
2018-04-24 10:31:08 -0700

    Create modal component

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { closeModal } from '../../actions/modal_actions';
+import { connect } from 'react-redux';
+import LoginFormContainer from '../session_form/login_form_container';
+import SignupFormContainer from '../session_form/signup_form_container';
+
+function Modal({modal, closeModal}) {
+  if (!modal) {
+    return null;
+  }
+
+  return (
+    <div className="modal-background" onClick={closeModal}>
+      <div className="modal-child" onClick={e => e.stopPropagation()}>
+        {/* modal is a component */}
+        { modal }
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    modal: state.ui.modal
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
