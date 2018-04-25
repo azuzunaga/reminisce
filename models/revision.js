7a08d382654c587bc2737eddbeb95d572cd135aa
Americo <ame.zuz@gmail.com>
2018-04-25 10:24:30 -0700

    Add empty object as default body

--- models/revision.js
+++ models/revision.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const revisionSchema = new Schema({
-  body: { type: Object, required: true },
+  body: { type: Object, default: {}, required: true },
   title: { type: String, required: true },
   userId: { type: Schema.Types.ObjectId, ref: "users", required: true },
   createdAt: { type: Date, default: Date.now, required: true }
