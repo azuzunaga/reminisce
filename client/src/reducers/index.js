797d31ccaa4dc7e3be1ec8b3bcef5e82a5bce72d
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-24 19:40:51 -0700

    put projectsReducer into rootreducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import uiReducer from './uiReducer';
+import projectReducer from './projectReducer';
 
 export default combineReducers({
   auth: authReducer,
+  projects: projectReducer,
   ui: uiReducer
 });
