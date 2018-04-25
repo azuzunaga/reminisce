210ab0dfe25aeb88453372ca3f6aad8d781f4302
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-24 21:01:19 -0700

    project list item now displays users first name

--- client/src/components/ProjectsListItem.jsx
+++ client/src/components/ProjectsListItem.jsx
@@ -6,15 +6,14 @@ import '../styles/stylingList.css';
 class ProjectsListItem extends React.Component {
   render() {
 
-    const { project } = this.props;
-
+    const { project, users } = this.props;
     return (
       <li>
         <div className='project-name'>
           <p> {project.name} </p>
         </div>
         <div className='project-details'>
-          <p>{project.owner}</p>
+          <p>{users[project.ownerId].firstName}</p>
           <p>{project.createdAt.split("T")[0]}</p>
           <p>{project.modifiedBy}</p>
         </div>
@@ -25,8 +24,8 @@ class ProjectsListItem extends React.Component {
 
 
 
-function mapStateToProps({ auth }) {
-  return { auth };
+function mapStateToProps(state) {
+  return { auth: state.auth, users: state.users };
 }
 
 export default connect(mapStateToProps)(ProjectsListItem);
