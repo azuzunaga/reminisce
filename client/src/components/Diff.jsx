ddc23c6feaa51a37e8255911214dd799805b8ca0
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-25 12:36:30 -0700

    Update Diff component to display each changed file in a save and whether it was inserted, edited, or deleted

--- client/src/components/Diff.jsx
+++ client/src/components/Diff.jsx
@@ -1,20 +1,61 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { pickBy } from 'lodash';
 
-import diff from '../utils/diff';
+import { fetchSave } from '../actions';
+import diffSaves from '../utils/diff';
 
 class Diff extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
   componentDidMount() {
-    // Fetch commit and prev commit
+    this.props
+      .fetchSave(this.props.saveId)
+      .then(() => this.setState({ loading: false }));
   }
 
   render() {
-    const { newSave } = this.props;
+    if (this.state.loading) {
+      return <div className="loading" />;
+    }
+    if (!this.props.save) debugger;
+    const { save, prevSave, changedRevisions } = this.props;
     return (
       <div className="diff-view">
-        <h2>Save:</h2>
+        <h3>Save: {save.name}</h3>
+        <ul>
+          {changedRevisions.map(rev => (
+            <div>
+              {rev.title}: {rev.change}
+            </div>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-export default Diff;
+const mapStateToProps = (state, ownProps) => {
+  const save = state.saves[ownProps.saveId];
+  if (!save) return {};
+  const prevSave = state.saves[save.previousSaveId] || { revisionIds: [] };
+  const revisions = pickBy(
+    state.revisions,
+    (rev, id) =>
+      prevSave.revisionIds.includes(id) || save.revisionIds.includes(id)
+  );
+  const changedRevisions = diffSaves(prevSave, save, revisions);
+  return {
+    save,
+    prevSave,
+    changedRevisions
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  fetchSave: id => dispatch(fetchSave(id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Diff);
