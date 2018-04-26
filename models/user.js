cd8ffbf4778d7a46a07cde5f0991b7093d06f335
Americo <ame.zuz@gmail.com>
2018-04-25 20:27:35 -0700

    Add active draft sub document

--- models/user.js
+++ models/user.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const activeDraftSchema = new Schema({
+  projectId: {type: Schema.Types.ObjectId, ref: "projects", require: true},
+  activeDraftId: {type: Schema.Types.ObjectId, ref: "drafts", require: true}
+});
+
 const userSchema = new Schema({
   googleId: { type: String, index: { unique: true } },
   username: { type: String, required: true, index: { unique: true } },
   firstName: String,
   lastName: String,
-  imageUrl: String
+  imageUrl: String,
+  projectsActiveDraft: [activeDraftSchema]
 });
 
 mongoose.model("users", userSchema);
