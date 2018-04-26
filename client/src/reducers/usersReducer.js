72bfa6de45b35c80f7a7e8a164ccfd41796c7fa4
youknowhu <kimberly.hu@gmail.com>
2018-04-25 21:45:29 -0700

    fetch save history with users

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,10 +1,12 @@
-import { FETCH_PROJECTS } from '../actions/types';
+import { FETCH_PROJECTS, FETCH_DRAFT } from '../actions/types';
 import merge from 'lodash/merge';
 
 export default function(state={}, action) {
   switch (action.type) {
     case FETCH_PROJECTS:
       return merge({}, state, action.users);
+    case FETCH_DRAFT:
+      return merge({}, state, action.users)
     default:
       return state;
   }
