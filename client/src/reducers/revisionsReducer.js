d7a7201c0d6050eccf4278efd2cfbc60f48cd2f3
Americo <ame.zuz@gmail.com>
2018-04-26 19:15:01 -0700

    Handle active draft revisions and merge fetched revision

--- client/src/reducers/revisionsReducer.js
+++ client/src/reducers/revisionsReducer.js
@@ -1,22 +1,31 @@
 import { merge, mapValues } from 'lodash';
 
-import { FETCH_SAVE, FETCH_REVISION, FETCH_PROJECT } from '../actions/types';
+import {
+  FETCH_SAVE,
+  FETCH_REVISION,
+  FETCH_PROJECT,
+  FETCH_DRAFT
+} from '../actions/types';
 
 export default (state = {}, action) => {
+  let fetchedRev;
   switch (action.type) {
-    case FETCH_SAVE:
-      return merge(
-        {},
-        state,
-        mapValues(action.revisions, rev =>
-          merge({ body: { entityMap: {} } }, rev)
-        )
-      );
-    case FETCH_REVISION:
-      return merge({}, state, action.revision);
-    case FETCH_PROJECT:
-      return merge({}, state, action.revisions);
-    default:
-      return state;
+  case FETCH_SAVE:
+    return merge(
+      {},
+      state,
+      mapValues(action.revisions, rev =>
+        merge({ body: { entityMap: {} } }, rev)
+      )
+    );
+  case FETCH_REVISION:
+    fetchedRev = { [action.revision.id]: action.revision };
+    return merge({}, state, fetchedRev);
+  case FETCH_PROJECT:
+    return merge({}, state, action.revisions);
+  case FETCH_DRAFT:
+    return merge({}, state, action.revisions);
+  default:
+    return state;
   }
 };
