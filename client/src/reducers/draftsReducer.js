9e8fec2ddbd7d15d2a425dc9fe77af3d9e638807
Americo <ame.zuz@gmail.com>
2018-04-26 22:12:59 -0700

    Add CREATE_PROJECT action in drafts reducer

--- client/src/reducers/draftsReducer.js
+++ client/src/reducers/draftsReducer.js
@@ -3,7 +3,8 @@ import { merge } from 'lodash';
 import {
   FETCH_PROJECT,
   CREATE_DRAFT,
-  FETCH_SAVE
+  FETCH_SAVE,
+  CREATE_PROJECT
 } from '../actions/types';
 
 export default (state = {}, action) => {
@@ -11,6 +12,9 @@ export default (state = {}, action) => {
   switch (action.type) {
   case FETCH_PROJECT:
     return merge({}, state, action.drafts);
+  case CREATE_PROJECT:
+    newDraft = { [action.draft.id]: action.draft };
+    return merge({}, state, newDraft);
   case CREATE_DRAFT:
     newDraft = { [action.draft.id]: action.draft };
     return merge({}, state, newDraft);
