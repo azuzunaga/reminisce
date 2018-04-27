dd9ba3f527cab4ae10f74dab3fd1f17c1b695f93
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-26 18:52:35 -0700

    update revision reducer to give us our new state

--- client/src/reducers/revisionsReducer.js
+++ client/src/reducers/revisionsReducer.js
@@ -5,13 +5,7 @@ import { FETCH_SAVE, FETCH_REVISION, FETCH_PROJECT } from '../actions/types';
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_SAVE:
-      return merge(
-        {},
-        state,
-        mapValues(action.revisions, rev =>
-          merge({ body: { entityMap: {} } }, rev)
-        )
-      );
+      return merge({}, state, action.revisions);
     case FETCH_REVISION:
       return merge({}, state, action.revision);
     case FETCH_PROJECT:
