ed4b0d6c5dac1a8fff19b5240911b100d6c94c84
Americo <ame.zuz@gmail.com>
2018-04-26 19:02:11 -0700

    Add new draft to redux state

--- client/src/reducers/draftsReducer.js
+++ client/src/reducers/draftsReducer.js
@@ -1,12 +1,19 @@
 import { merge } from 'lodash';
 
-import { FETCH_PROJECT } from '../actions/types';
+import {
+  FETCH_PROJECT,
+  CREATE_DRAFT,
+} from '../actions/types';
 
 export default (state = {}, action) => {
+  let newDraft;
   switch (action.type) {
-    case FETCH_PROJECT:
-      return merge({}, state, action.drafts);
-    default:
-      return state;
+  case FETCH_PROJECT:
+    return merge({}, state, action.drafts);
+  case CREATE_DRAFT:
+    newDraft = { [action.draft.id]: action.draft };
+    return merge({}, state, newDraft);
+  default:
+    return state;
   }
 };
