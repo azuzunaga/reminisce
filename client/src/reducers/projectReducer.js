e7401b2b9d88789ebc8228f77eba729cc426e16c
Americo <ame.zuz@gmail.com>
2018-04-27 16:43:40 -0700

    Hit the project reducer when the CREATE_DRAFT action is triggered

--- client/src/reducers/projectReducer.js
+++ client/src/reducers/projectReducer.js
@@ -1,17 +1,24 @@
 import {
   FETCH_PROJECTS,
   FETCH_PROJECT,
-  CREATE_PROJECT
+  CREATE_PROJECT,
+  CREATE_DRAFT
 } from '../actions/types';
 import merge from 'lodash/merge';
 export default function(state={}, action) {
+  let project;
   switch (action.type) {
     case FETCH_PROJECTS:
       return merge({}, action.projects);
     case FETCH_PROJECT:
       return merge({}, state, {[action.project._id]: action.project});
     case CREATE_PROJECT:
-    return merge({}, state, {[action.project._id]: action.project});
+      return merge({}, state, {[action.project._id]: action.project});
+    case CREATE_DRAFT:
+      project = merge({}, state[action.draft.projectId]);
+      project.draftIds.push(action.draft._id);
+      return merge({}, state, {[project._id]: project});
+      break;
     default:
       return state;
   }
