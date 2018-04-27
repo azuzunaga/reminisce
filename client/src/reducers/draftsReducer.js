8396486655e03f208029bb466ab2cdca5abb83a4
Gabriel Talavera <gabrielvt6@gmail.com>
2018-04-26 18:52:20 -0700

    update draft reducer to update with fetch save and include new save

--- client/src/reducers/draftsReducer.js
+++ client/src/reducers/draftsReducer.js
@@ -1,11 +1,17 @@
 import { merge } from 'lodash';
 
-import { FETCH_PROJECT } from '../actions/types';
+import { FETCH_PROJECT, FETCH_SAVE } from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_PROJECT:
       return merge({}, state, action.drafts);
+    case FETCH_SAVE:
+      let newState = Object.assign({}, state);
+      let draft = newState[action.save.draftId];
+      newState[draft._id] = merge({}, draft,
+        {saveIds: draft.saveIds.concat([action.save._id])});
+      return newState;
     default:
       return state;
   }
