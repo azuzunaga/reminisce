9895825576ed44048c8d3b5c5fa1285c8e7b22e3
Americo <ame.zuz@gmail.com>
2018-04-29 16:37:29 -0700

    Display the right data and style

--- client/src/components/ProjectFileTree.jsx
+++ client/src/components/ProjectFileTree.jsx
@@ -4,29 +4,48 @@ import { Link } from 'react-router-dom';
 import '../styles/projectFileTree.css';
 
 class ProjectFileTree extends React.Component {
-
   menu() {
+    console.log(this.props.documentId);
+    let revisions = Object.values(this.props.revisions);
     if (this.props.view === 'DocumentForm') {
       return (
-        <ul className="project-file-tree-list">
-          {this.props.revisions.map(doc => {
-            return (
-              <li>
-                <Link
-                  to={`/project/${this.props.project._id}/document/${doc._id}`}
-                >
-                  <i class="material-icons">chevron_right</i>
-                  <h6>{doc.title}</h6>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+        <div>
+          <div className="folder-header">
+            <Link to={`/projects/${this.props.projectId}`}>
+              <i className="material-icons">chevron_right</i>
+              <h4>{this.props.projectName}</h4>
+            </Link>
+          </div>
+          <ul className="project-file-tree-list">
+            {revisions.map(doc => {
+              if (doc._id === this.props.documentId) {
+                return (
+                  <li className='current-doc' key={doc._id}>
+                    <i className="material-icons">chevron_right</i>
+                    <h6>{doc.title}</h6>
+                  </li>
+                );
+              } else {
+                return (
+                  <li key={doc._id}>
+                    <Link
+                      to={`/project/${this.props.projectId}/document/${doc._id}`}
+                    >
+                      <i className="material-icons">chevron_right</i>
+                      <h6>{doc.title}</h6>
+                    </Link>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </div>
       );
     } else {
       return null;
     }
   }
+  // }
 
 
   render() {
@@ -38,9 +57,7 @@ class ProjectFileTree extends React.Component {
             <i className="material-icons">chevron_left</i>
           </Link>
         </header>
-        <div>
-          {this.menu()}
-        </div>
+        {this.menu()}
       </div>
     );
   }
