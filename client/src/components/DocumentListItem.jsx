1169d72af249abcfbb849d56eff17ba72f84abd6
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-29 01:47:32 -0700

    Move logic for deleting documents into the DocumentListItem component

--- client/src/components/DocumentListItem.jsx
+++ client/src/components/DocumentListItem.jsx
@@ -3,14 +3,14 @@ import { connect } from 'react-redux';
 import '../styles/stylingList.css';
 import { dateTimeFormatter } from '../utils/dateFormatter';
 import { Link } from 'react-router-dom';
-import { openModal } from '../actions';
+import { openModal, createSave } from '../actions';
 import TitleEditForm from './TitleEditForm';
 import ConfirmDeleteForm from './ConfirmDeleteForm';
 import pencil from '../assets/pencil-edit-button.png';
 class DocumentListItem extends React.Component {
   render() {
 
-    const { doc, user, projectId } = this.props;
+    const { doc, user, projectId, draftId } = this.props;
 
     return (
       <li className='list-item'>
@@ -32,7 +32,9 @@ class DocumentListItem extends React.Component {
             className="delete-button"
             onClick={
               () => this.props.openModal(
-                <ConfirmDeleteForm revisionId={doc._id} projectId={projectId} />
+                <ConfirmDeleteForm
+                  message="Are you sure you want to delete this document?"
+                  action={this.props.deleteDocument(doc, draftId)} />
               )
             }>
             ×
@@ -49,12 +51,29 @@ class DocumentListItem extends React.Component {
 
 
 
-function mapStateToProps({ auth }) {
-  return { auth };
+function mapStateToProps(state, ownProps) {
+  let activeDraftArr = state.auth.projectsActiveDraft;
+  let idx = activeDraftArr.findIndex(
+    el => el.projectId === ownProps.projectId
+  );
+  let draftId = activeDraftArr[idx].draftId;
+  return {
+    auth: state.auth,
+    draftId
+  };
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  openModal: (component) => dispatch(openModal(component))
+  openModal: (component) => dispatch(openModal(component)),
+  deleteDocument: (revision, draftId) => () => {
+    const save = Object.assign({}, {
+      save: { name: `Delete Document: ${revision.title}`,
+        draftId: draftId, isAuto: false },
+        newRevs: [],
+        deletedRevIds: [revision._id]
+    });
+    return dispatch(createSave(save));
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DocumentListItem);
