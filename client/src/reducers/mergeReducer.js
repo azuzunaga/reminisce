19e61d286ad4f25ad521aa4dc9173f5bb0ce9252
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-30 00:32:23 -0700

    Set up loading state in merge reducer

--- client/src/reducers/mergeReducer.js
+++ client/src/reducers/mergeReducer.js
@@ -1,16 +1,23 @@
 import { merge } from 'lodash';
 
-import { FETCH_MERGE, SET_ALL_CONFLICTS } from '../actions/types';
+import {
+  FETCH_MERGE,
+  SET_ALL_CONFLICTS,
+  MERGE_LOADING
+} from '../actions/types';
 
-export default function mergeReducer(state = {}, action) {
+export default function mergeReducer(state = { loading: true }, action) {
   switch (action.type) {
     case FETCH_MERGE:
-      return action.data;
+      return merge({}, action.data, { loading: false });
     case SET_ALL_CONFLICTS:
       return merge({}, state, {
         revisions: action.revisions,
-        chunkedMerges: action.chunkedMerges
+        chunkedMerges: action.chunkedMerges,
+        loading: false
       });
+    case MERGE_LOADING:
+      return merge({}, state, { loading: true });
     default:
       return state;
   }
