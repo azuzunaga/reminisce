c8ad7e8fee5b97d795baf588d2e80402c0dfc939
Ian MacLeod <ian.a.macleod@gmail.com>
2018-04-29 22:27:19 -0700

    Fix bugs when navigating to project dashboard using back button

--- client/src/utils/authRoutes.js
+++ client/src/utils/authRoutes.js
@@ -2,34 +2,38 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, Redirect, withRouter } from "react-router-dom";
 
-const Auth = ({ component: Component, path, loggedIn, exact }) => {
+const Auth = ({ component: Component, path, user, exact }) => {
+  if (user === null) return '';
   return (
   <Route
     path={path}
     exact={exact}
     render={props =>
-      !loggedIn ? <Component {...props} /> : <Redirect to="/dashboard" />
+      !user ? <Component {...props} /> : <Redirect to="/dashboard" />
     }
   />
   );
 };
 
-const Protected = ({ component: Component, path, loggedIn, exact }) => (
-  <Route
-    path={path}
-    exact={exact}
-    render={props =>
-      loggedIn ? <Component {...props} /> : <Redirect to="/" />
+const Protected = ({ component: Component, path, user, exact }) => {
+  if (user === null) return '';
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={props =>
+        user ? <Component {...props} /> : <Redirect to="/" />
     }
-  />
-);
+    />
+  );
+};
 
 const mapStateToPropsAuth = state => {
-  return { loggedIn: !!state.auth };
+  return { user: state.auth };
 };
 
 const mapStateToPropsProtected = state => {
-  return { loggedIn: state.auth !== false };
+  return { user: state.auth };
 };
 
 export const AuthRoute = withRouter(connect(mapStateToPropsAuth)(Auth));
