68ff1a50fc69dce4faddb947ed0bcfd4ce40b09e
Ian MacLeod <ian.a.macleod@gmail.com>
2018-05-03 11:49:25 -0700

    Change revision route to use title instead of id

--- routes/revisionRoutes.js
+++ routes/revisionRoutes.js
@@ -4,16 +4,32 @@ const _ = require('lodash');
 const Revision = mongoose.model('revisions');
 const Project = mongoose.model('projects');
 const Draft = mongoose.model('drafts');
+const Save = mongoose.model('saves');
 
 module.exports = app => {
-  app.get('/api/projects/:projectId/revisions/:id', async (req, res) => {
-    const revision = await Revision.findById(req.params.id);
+  app.get('/api/projects/:projectId/revisions/:title', async (req, res) => {
     const project = await Project.findById(req.params.projectId);
     const projectDraft = _.find(
       req.user.projectsActiveDraft,
       el => el.projectId.toString() === project.id.toString()
     );
     const draft = await Draft.findById(projectDraft.draftId);
+    const saveId = draft.saveIds[draft.saveIds.length - 1];
+    const lastSave = await Save.findById(saveId);
+    const revisions = await Revision.find({ _id: { $in: lastSave.revisionIds } }) || [];
+
+    console.log(revisions);
+    const revision = _.find(
+      revisions,
+      rev => rev.title == req.params.title
+    )
+
+    if (!revision) {
+      return res.status(404).json(["That document does not exist"]);
+    }
+
     res.json({ revision, project, draft });
   });
+
+
 };
