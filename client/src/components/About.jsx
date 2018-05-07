c08fb743ffca7aa7e924bcc1ff25f72f449fe61e
youknowhu <kimberly.hu@gmail.com>
2018-05-07 16:59:32 -0700

    updated about me and added google analytics

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,6 +4,11 @@ import ghBlack from '../assets/githubBlack.png';
 import ghRed from '../assets/githubRed.png';
 import lnBlack from '../assets/lnBlack.png';
 import lnBlue from '../assets/lnBlue.png';
+import kimmyHeadshot from '../assets/kimmyHeadshot.jpg';
+import gabrielHeadshot from '../assets/gabrielHeadshot.jpg';
+import ianHeadshot from '../assets/ianHeadshot.jpg';
+import americoHeadshot from '../assets/americoHeadshot.jpg';
+
 
 class About extends React.Component {
   render () {
@@ -15,12 +20,19 @@ class About extends React.Component {
         <ul className="dev-list">
           <li className="gabriel">
             <h3>Gabriel Talavera</h3>
-            <p>Gabriel has never written one line of bad code.
-              His greatest weaknesses are that he works too hard
-              and he cares too much. When Gabriel isn't referring to
-              himself in the third person or coding he enjoys writing,
-              watching movies and long walks on the beach
-            </p>
+            <div className='bio-content'>
+              <div className='about-left'>
+                <img src={gabrielHeadshot}
+                  className='about-headshot'/>
+
+              </div>
+              <p>Gabriel has never written one line of bad code.
+                His greatest weaknesses are that he works too hard
+                and he cares too much. When Gabriel isn't referring to
+                himself in the third person or coding he enjoys writing,
+                watching movies and long walks on the beach.
+              </p>
+            </div>
             <ul className="contact-info">
               <li>
                 <a href="https://github.com/gabrieltal"
@@ -47,7 +59,16 @@ class About extends React.Component {
 
           <li className="ian">
             <h3>Ian MacLeod</h3>
-            <p>Bio</p>
+            <div className='bio-content'>
+              <div className='about-left'>
+                <img src={ianHeadshot}
+                  className='about-headshot'/>
+
+              </div>
+              <p>Ian wrote his first line of code when he was just ten years old. Since then, he has been fascinated by the process of coding.
+              The moments when the solution to a tricky problem or an elusive bug clicks into place make him lose track of time and code for hours at a time.
+              Right now his tools of choice are Ruby, Rails, Python, Flask, Django, React, and Redux, but he loves to pick up new languages and technologies, too.</p>
+            </div>
             <ul className="contact-info">
               <li>
                 <a href="https://github.com/Ian-MacLeod"
@@ -72,7 +93,17 @@ class About extends React.Component {
 
           <li className="americo">
             <h3>Americo Zuzunaga</h3>
-            <p>Bio</p>
+
+            <div className='bio-content'>
+              <div className='about-left'>
+                <img src={americoHeadshot}
+                  className='about-headshot'/>
+
+              </div>
+              <p> Americo loves figuring out how things work - from rebuilding the brake calipers of his motorcycle, to integrating a new API into one of his projects, or learning the latest frontend library.
+              He fell in love with coding on first sight; what started as a chance encounter with R and statistics has blossomed into full stack web applications and JS games and visualizations.
+              When not in front of a computer, you can find him on a bicycle exploring the Marin Headlands, hiking, or cozied up on the couch with a book.</p>
+            </div>
             <ul className="contact-info">
               <li>
                 <a href="https://github.com/azuzunaga"
@@ -98,8 +129,20 @@ class About extends React.Component {
 
 
           <li className="kimberly">
-            <h3>Kimberly Allgeier</h3>
-            <p>Bio</p>
+            <h3>Kimmy Allgeier</h3>
+            <div className='bio-content'>
+              <div className='about-left'>
+                <img src={kimmyHeadshot}
+                  className='about-headshot'/>
+
+              </div>
+
+              <p>Kimmy is passionate about user-centric design. With past experience in implementation and product management,
+              she has experienced first-hand the importance of deeply understanding the customer when building a feature. She is excited to continue building and creating awesome user expriences.
+              When not in front of a computer, Kimmy loves exploring the outdoors. She is currently on a mission to visit all the U.S. national parks. Next up: Channel Islands! </p>
+
+            </div>
+
             <ul className="contact-info">
               <li>
                 <a href="https://github.com/youknowhu"
